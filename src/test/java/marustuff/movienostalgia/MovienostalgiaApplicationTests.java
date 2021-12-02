6e44d2992f6614ace5b820c425c3d040b334369c
maru-stuff <norepy@example.com>
2021-12-02 20:18:38 +0100

    Initial commit

---
+++ src/test/java/marustuff/movienostalgia/MovienostalgiaApplicationTests.java
@@ -0,0 +1,13 @@
+package marustuff.movienostalgia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovienostalgiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
